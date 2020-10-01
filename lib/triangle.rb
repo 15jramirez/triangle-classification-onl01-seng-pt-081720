c9cfac3b3cff4367df95a4c19d39f9f90c0f1e7a
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-10-01 05:00:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
  
  def kind 
    if @length_one <= 0 && @length_two <= 0 && @length_three <= 0
-     raise 
+     raise TriangleError
    elsif @length_one == @length_two && @length_two == @length_three
      :equilateral
    elsif @length_one == @length_two || @length_two == @length_three || @length_three == @length_one
