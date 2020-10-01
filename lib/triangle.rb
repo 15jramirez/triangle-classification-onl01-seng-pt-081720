329a1bc277a06cfed71380173aaaf906c3bec7eb
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-10-01 05:01:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
  end
  
  def kind 
-   if @length_one <= 0 && @length_two <= 0 && @length_three <= 0
+   if @length_one <= 0 && @length_two <= 0 && @length_three <= 0 || 
      raise TriangleError
    elsif @length_one == @length_two && @length_two == @length_three
      :equilateral
