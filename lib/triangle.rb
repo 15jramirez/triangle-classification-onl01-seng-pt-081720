a6180d690449389edd1863e1f417f4934e42ccea
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-10-01 05:04:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,8 +14,6 @@ class Triangle
       rescue PartnerError => error
           puts error.message
       end
-    elsif
-    
    elsif @length_one == @length_two && @length_two == @length_three
      :equilateral
    elsif @length_one == @length_two || @length_two == @length_three || @length_three == @length_one
